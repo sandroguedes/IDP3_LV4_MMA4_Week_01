0d8fb089b43ee8bc86437b6e3d0b28d9c4def954
sandroguedes <59712770+sandroguedes@users.noreply.github.com>
2021-01-19 13:21:00 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http'); // like a PHP require
+const stream = require('fs');
 
 // require is more or less the same as JS import
 // const hostname = '127.0.0.1'; // this is localhost
@@ -6,8 +7,36 @@ const port = process.env.PORT || 3000; // localhost:3000
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Sup World, HOW U DOIN');
+    res.setHeader('Content-Type', 'text/html');
+
+    let url = req.url; // i.e. localhost:3000/contact.html
+
+    switch (url) {
+        case "/contact":
+            stream.readFile('contact.html', null, ((err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write('404 not found')
+                } else {
+                    res.write(data);
+                }
+            }))
+        break;
+
+        case "/portfolio":
+            stream.readFile('portfolio.html', null, ((err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write('404 not found')
+                } else {
+                    res.write(data);
+                }
+            }))
+        break;
+
+        default:
+        res.end('Sup World, HOW U DOIN');
+    }
 });
 
 server.listen(port, () => {
